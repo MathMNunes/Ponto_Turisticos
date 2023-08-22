59c376251a480539c29ff247b34f7a7b3503d17a
MathMNunes <matheusmarques9@hotmail.com>
2023-08-22 19:35:42 -0300

    Explorando as actions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,7 +1,38 @@
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
+
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        return PontoTuristico.objects.filter(aprovado=True)
+
+    # def list(self, request, *args, **kwargs):
+    #     return Response({'teste': 123})
+    
+    # def create(self, request, *args, **kwargs):
+    #     return Response({'Hello' : request.data['nome']})
+    
+    # def destroy(self, request, *args, **kwargs):
+    #     return Response({'Deletado com sucesso' : 123})
+    
+    # def retrieve(self, request, *args, **kwargs):
+    #     return Response({'opa' : 123})
+    
+    # def update(self, request, *args, **kwargs):
+    #     return Response({'Atualizado com sucesso' : 123})
+    
+    # def partial_update(self, request, *args, **kwargs):
+    #     return Response({'Foi parcialmente atualizado' : 123})
+
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+        return Response({'teste': 123})
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request, pk=None):
+        return Response({'teste': 123})
