00d2388e68b3fb029feed46261c62fab581f6974
MathMNunes <matheusmarques9@hotmail.com>
2023-08-23 17:17:31 -0300

    Feito Estudo de pesquisa e filtros

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -7,27 +8,48 @@ from .serializers import PontoTuristicoSerializer
 class PontoTuristicoViewSet(ModelViewSet):
 
     serializer_class = PontoTuristicoSerializer
+    filter_backends = [SearchFilter]
+    #Podemos pegar um dado por uma chave estrangeira que nesse caso vai ser o endereco!!
+    search_fields = ['nome', 'descricao', 'endereco__linha1']
+    #Podemos trocar o padrão do REST do id para o nome usando o lookup_field
+    lookup_field = 'nome'
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
 
-    # def list(self, request, *args, **kwargs):
-    #     return Response({'teste': 123})
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset = queryset.filter(nome_iexact=nome)
+
+        if descricao:
+            queryset = queryset.filter(descricao_iexact=descricao)
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
     
-    # def create(self, request, *args, **kwargs):
-    #     return Response({'Hello' : request.data['nome']})
+    def create(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
     
-    # def destroy(self, request, *args, **kwargs):
-    #     return Response({'Deletado com sucesso' : 123})
+
+    def destroy(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
     
-    # def retrieve(self, request, *args, **kwargs):
-    #     return Response({'opa' : 123})
+    def retrieve(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
     
-    # def update(self, request, *args, **kwargs):
-    #     return Response({'Atualizado com sucesso' : 123})
+    def update(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
     
-    # def partial_update(self, request, *args, **kwargs):
-    #     return Response({'Foi parcialmente atualizado' : 123})
+    def partial_update(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
     @action(methods=['get'], detail=True)
     def denunciar(self, request, pk=None):
